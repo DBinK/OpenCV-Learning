3a34b26afc9d40134c70393941ad80fb004ce572
DBin_K <dbinkv1@gmail.com>
2023-07-30 22:03:15 +0800

    uuuuuuuupdate

--- 2.1.显示图像.cpp
+++ 2.1.显示图像.cpp
@@ -1,14 +1,14 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp> // 包含 OpenCV 库文件
 
-using namespace cv;
+using namespace cv; // 使用 OpenCV 命名空间
 
 int main() {
-    Mat image = imread("example.jpg");
-    if (image.empty()) {
-        std::cout << "无法加载图像文件." << std::endl;
-        return -1;
+    Mat image = imread("example.jpg"); // 加载图像文件到 Mat 对象中
+    if (image.empty()) { // 如果图像为空（无法加载）
+        std::cout << "无法加载图像文件." << std::endl; // 输出错误信息
+        return -1; // 返回错误码
     }
-    imshow("图像", image);
-    waitKey(0);
-    return 0;
+    imshow("图像", image); // 在窗口中显示加载的图像
+    waitKey(0); // 等待用户按下按键
+    return 0; // 返回正常退出码
 }
