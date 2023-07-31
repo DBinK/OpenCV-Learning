8f8399fd9dd491b70647877cdc9706eee012ca67
DBin_K <DBinKv1@Gmail.com>
2023-07-31 11:19:54 +0800

    Update 3.2.指针的使用.cpp

--- 3.2.指针的使用.cpp
+++ 3.2.指针的使用.cpp
@@ -3,29 +3,37 @@
 题目二：指针的使用 请编写一个函数，接受两个整数作为参数，并交换这两个整数的值。使用指针来实现交换。
 
 */
+
+
 #include <iostream>
 
+// 定义一个交换函数，使用指针作为参数
 void swap(int* a, int* b) {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+    // 交换a和b指向的值
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int main() {
-  int num1 = 10;
-  int num2 = 20;
+    // 初始化两个变量
+    int num1 = 10;
+    int num2 = 20;
 
-  std::cout << "交换前：" << std::endl;
-  std::cout << "num1 = " << num1 << std::endl;
-  std::cout << "num2 = " << num2 << std::endl;
+    // 输出交换前的值
+    std::cout << "交换前：" << std::endl;
+    std::cout << "num1 = " << num1 << std::endl;
+    std::cout << "num2 = " << num2 << std::endl;
 
-  swap(&num1, &num2);
+    // 调用交换函数，传递num1和num2的地址
+    swap(&num1, &num2);
 
-  std::cout << "交换后：" << std::endl;
-  std::cout << "num1 = " << num1 << std::endl;
-  std::cout << "num2 = " << num2 << std::endl;
+    // 输出交换后的值
+    std::cout << "交换后：" << std::endl;
+    std::cout << "num1 = " << num1 << std::endl;
+    std::cout << "num2 = " << num2 << std::endl;
 
-  return 0;
+    return 0;
 }
 
 /*
